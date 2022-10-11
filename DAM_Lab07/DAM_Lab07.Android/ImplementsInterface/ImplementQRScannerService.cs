7b7b8df2135658fb1ee97e541b95706bf1506dc4
Diego Flores <floresdiego2001xd@gmail.com>
2022-10-11 16:46:54 -0500

    Agregar archivos de proyecto.

---
+++ DAM_Lab07/DAM_Lab07.Android/ImplementsInterface/ImplementQRScannerService.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using DAM_Lab07.Droid.ImplementsInterface;
+using DAM_Lab07.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+[assembly: Dependency(typeof(ImplementQRScannerService))]
+namespace DAM_Lab07.Droid.ImplementsInterface
+{
+    public class ImplementQRScannerService : IQRScannerService
+    {
+        public async Task<string> ReadQrScanner()
+        {
+            try
+            {
+                var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+                scanner.TopText = "Lectura de QR";
+                scanner.BottomText = "Please Wait";
+                var result = await scanner.Scan();
+                return result.Text;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+                return e.Message;
+            }
+        }
+    }
+}
---
+++ DAM_Lab07/DAM_Lab07/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using DAM_Lab07.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace DAM_Lab07
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /*private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await Navigation.PushModalAsync(scan);
+
+            scan.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopModalAsync();
+                    await DisplayAlert("Valor QRCODE", "" + result.Text, "OK");
+                });
+            };
+        }*/
+
+        private async void btnScannerQR_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var scanner = DependencyService.Get<IQRScannerService>();
+                var result = await scanner.ReadQrScanner();
+
+                if(result != null)
+                {
+                    txtResultado.Text = result;
+                }
+            }catch(Exception ex)
+            {
+                txtResultado.Text=ex.Message;
+            }
+        }
+    }
+}
